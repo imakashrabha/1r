eb4465e38f2586b04d3c3943b0ff4d4027e2abd5
dy3no <165803560+dy3no@users.noreply.github.com>
2024-12-21 13:39:39 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -67,8 +67,5 @@ async def link_generator(client: Client, message: Message):
 
 
 
-# Jishu Developer 
+# Akash Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
