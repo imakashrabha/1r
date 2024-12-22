9d72f864328abf9016f955c0216eb31ccf2f43ce
dy3no <165803560+dy3no@users.noreply.github.com>
2024-12-22 14:04:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🧑‍💻 ᴅᴇᴠʟᴏᴘᴇʀ :</b> <a href='tg://user?id={OWNER_ID}'>ᴛʜɪs ᴘᴇʀsᴏɴ</a><b>",
+            text = f"<b>🧑‍💻 ᴅᴇᴠʟᴏᴘᴇʀ :</b> <a href='tg://user?id={OWNER_ID}'>ᴀᴋᴀsʜ</a><b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup([
                 [InlineKeyboardButton("ʜᴏᴍᴇ", callback_data = "home"),
