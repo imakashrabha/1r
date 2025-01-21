f5e7d0a7bcad7e215a936667c81c11a98d9787e0
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-01-21 22:41:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ START_MSG = os.environ.get("START_MESSAGE", "<b>Hᴇʟʟᴏ {mention}\n\nɪ ᴄ�
 #Force sub message 
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b><blockquote>›› ʜᴇʏ {mention} ×</blockquote> \n   ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ ᴊᴏɪɴ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, ᴊᴏɪɴ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs.</b>""")
+    "<b>›› ʜᴇʏ {mention} × \n\n   ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ ᴊᴏɪɴ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, ᴊᴏɪɴ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs.</b>""")
 
 
 
