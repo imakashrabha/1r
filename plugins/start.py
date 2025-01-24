f65c2a0111172764c01b284d3c54960976de52c2
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-01-24 14:09:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
            
-        k = await client.send_message(chat_id = message.from_user.id, text=f"𝖳𝗁𝖾 𝗅𝗂𝗇𝗄 𝗐𝗂𝗅𝗅 𝖻𝖾 𝖽𝖾𝗅𝖾𝗍𝖾𝖽 𝗂𝗇 {file_auto_delete}. 𝖩𝗈𝗂𝗇 𝖻𝖾𝖿𝗈𝗋𝖾 𝗂𝗍 𝗀𝖾𝗍𝗌 𝖽𝖾𝗅𝖾𝗍𝖾𝖽.")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"𝖳𝗁𝗂𝗌 𝗐𝗂𝗅𝗅 𝖻𝖾 𝖽𝖾𝗅𝖾𝗍𝖾𝖽 𝗂𝗇 {file_auto_delete}. 𝖥𝗈𝗋𝗐𝖺𝗋𝖽 𝗂𝗍 𝗍𝗈 𝗒𝗈𝗎𝗋 𝗌𝖺𝗏𝖾𝖽 𝗆𝖺𝗌𝗌𝖺𝗀𝖾𝗌 𝖻𝖾𝖿𝗈𝗋𝖾 𝖽𝗈𝗐𝗇𝗅𝗈𝖺𝖽𝗂𝗇𝗀.")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
@@ -220,8 +220,8 @@ async def delete_files(messages, client, k):
             await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
-    # await client.send_message(messages[0].chat.id, "ʏᴏᴜʀ ʟɪɴᴋ ʜᴀs ʙᴇᴇɴ ᴅᴇʟᴇᴛᴇᴅ")
-    await k.edit_text("ʏᴏᴜʀ ʟɪɴᴋ ʜᴀs ʙᴇᴇɴ ᴅᴇʟᴇᴛᴇᴅ")
+    # await client.send_message(messages[0].chat.id, "ʏᴏᴜʀ ᴠɪᴅᴇᴏ ɪs ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.")
+    await k.edit_text("ʏᴏᴜʀ ᴠɪᴅᴇᴏ ɪs ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.")
     
 
 
